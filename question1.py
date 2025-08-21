1ba414d29b5d68dc57536ddfbb793eb1b9413375
zhangym <wangyi_zym@126.com>
2025-08-21 15:39:21 +1000

    refactor repository & commit question1

--- question1.py
+++ question1.py
@@ -13,6 +13,8 @@ except Exception as e:
     eprint(e)
     exit(1)
 
+import heapq
+
 #########################
 # Debugger and visualizer options
 #########################
@@ -23,7 +25,7 @@ visualizer = False
 
 # If you want to test on specific instance, turn test_single_instance to True and specify the level and test number
 test_single_instance = False
-level = 0
+level = 1
 test = 0
 
 
@@ -33,6 +35,21 @@ test = 0
 # Return a list of (x,y) location tuples which connect the start and goal locations.
 #########################
 
+def manhattan_distance(pos1, pos2):
+    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
+def get_next_position(x, y, direction):
+    if direction == Directions.NORTH:
+        return (x - 1, y)
+    elif direction == Directions.EAST:
+        return (x, y + 1)
+    elif direction == Directions.SOUTH:
+        return (x + 1, y)
+    elif direction == Directions.WEST:
+        return (x, y - 1)
+    else:
+        return (x, y)
+    
 
 # This function return a list of location tuple as the solution.
 # @param start A tuple of (x,y) coordinates
@@ -42,39 +59,76 @@ test = 0
 # @param max_timestep The max timestep of this episode.
 # @return path A list of (x,y) tuple.
 def get_path(start: tuple, start_direction: int, goal: tuple, rail: GridTransitionMap, max_timestep: int):
-    ############
-    # Below is an dummy path finding implementation,
-    # which always choose the first available transition of current state.
-    #
-    # Replace these with your implementation and return a list of (x,y) tuple as your plan.
-    # Your plan should avoid conflicts with paths in existing_paths.
-    ############
-    path = []
-    loc = start
-    direction = start_direction
-    for t in range(0, int(max_timestep/10)):
-        path.append(loc)
-        if loc == goal:
-            break;
-        valid_transitions = rail.get_transitions(loc[0],loc[1],direction)
-        for i in range(0,len(valid_transitions)):
-            if valid_transitions[i]:
-                new_x=loc[0]
-                new_y=loc[1]
-                action = i
-                if action == Directions.NORTH:
-                    new_x -= 1
-                elif action == Directions.EAST:
-                    new_y += 1
-                elif action == Directions.SOUTH:
-                    new_x += 1
-                elif action == Directions.WEST:
-                    new_y -= 1
-                loc = (new_x,new_y)
-                direction = action
+    """ A* algorithm implementation """
 
-                break;
-    return path
+    open_set = []
+    heapq.heappush(open_set, (0, 0, start, start_direction, [start]))
+    visited = set()
+    g_score = {} # g_value set
+    g_score[start, start_direction] = 0
+    
+    while open_set:
+    
+        # pop best node
+        f_score, current_g, current_pos, current_direction, current_path = heapq.heappop(open_set)
+        
+        # check time limit
+        if len(current_path) >= max_timestep:
+            continue
+        
+        current_state = (current_pos, current_direction)
+        # check duplicate visits
+        if current_state in visited:
+            continue
+        visited.add(current_state)
+        
+        # goal-test
+        if current_pos == goal:
+            return current_path
+        
+        # get valid actions
+        valid_actions = rail.get_transitions(current_pos[0], current_pos[1], current_direction)
+        
+        # iterate all possible actions and calculate the new state
+        for new_direction in range(len(valid_actions)):
+        
+            if valid_actions[new_direction]:
+                # new position
+                new_pos = get_next_position(current_pos[0], current_pos[1], new_direction)
+                
+                # check boundary
+                if new_pos[0] < 0 or new_pos[0] >= rail.width or new_pos[1] < 0 or new_pos[1] >= rail.height:
+                    continue
+                
+                # new state
+                new_state = (new_pos, new_direction)
+
+                # check duplicate visits
+                if new_state in visited:
+                    continue
+                
+                # calculate new g with cost=1
+                new_g = current_g + 1
+                
+                # compare new_g and current g
+                if new_state not in g_score or new_g < g_score[new_state]:
+                    # update g value
+                    g_score[new_state] = new_g
+                    
+                    # heuristic cost
+                    h_score = manhattan_distance(new_pos, goal)
+                    
+                    # calculate f value
+                    f_score = new_g + h_score
+                    
+                    # append path
+                    new_path = current_path + [new_pos]
+                    
+                    # update queue
+                    heapq.heappush(open_set, (f_score, new_g, new_pos, new_direction, new_path))
+    
+    return None
+
 
 
 #########################
